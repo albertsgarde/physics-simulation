900db5343c446184f81161c39bd79c5176c84af7
Albert Garde <albertsgarde@gmail.com>
2024-05-06 22:50:39 +0200

    :sparkles: Separate graphics and logic threads

--- simulation-mq/src/bin/main.rs
+++ simulation-mq/src/bin/main.rs
@@ -1,33 +1,12 @@
-use std::iter;
-
-use macroquad::{
-    time,
-    window::{self, Conf},
+use std::{
+    collections::VecDeque,
+    sync::mpsc,
+    time::{Duration, Instant},
 };
+
+use macroquad::window::{self, Conf};
 use simulation::{Config, Event, Location, Particle, State, Vector};
 use simulation_mq::{draw, UiConfig};
-
-trait TickFunction: FnMut() {}
-
-impl<F> TickFunction for F where F: FnMut() {}
-
-struct Tick {
-    cur_delta: f32,
-}
-
-impl Tick {
-    pub fn new() -> Self {
-        Self { cur_delta: 0. }
-    }
-
-    pub fn elapse_time<F: TickFunction>(&mut self, delta: f32, mut tick_function: F) {
-        self.cur_delta += delta;
-        while self.cur_delta >= 1. {
-            tick_function();
-            self.cur_delta -= 1.;
-        }
-    }
-}
 
 fn window_conf() -> Conf {
     Conf {
@@ -44,31 +23,67 @@ async fn main() {
 
     let mut state = State::new(config);
 
-    let ui_config: UiConfig = UiConfig::new(400., Vector::new(0., 0.), 10., 3.);
+    let ui_config: UiConfig = UiConfig::new(4000., Vector::new(0., 0.), 10., 3.);
 
     let mut ui_state = ui_config.new_ui_state();
     ui_state.set_offset(ui_state.offset_from_mid_offset(Vector::new(0., 0.), &state));
 
-    let mut tick = Tick::new();
+    let (event_sender, event_receiver) = mpsc::channel();
+    let (state_sender, mut state_receiver) = watch::channel(state.clone());
+    let (tps_sender, mut tps_receiver) = watch::channel(0.);
 
-    let mut events: Vec<_> = iter::repeat(Event::AddParticle(Particle::new(
-        Location::new(32., 32.),
-        Vector::new(0., 1.),
-    )))
-    .take(1000)
-    .collect();
+    let _ = std::thread::spawn(move || {
+        let mut next_iteration = Instant::now();
+        let mut iteration_times = VecDeque::new();
+        loop {
+            iteration_times.push_back(Instant::now());
+            while let Some(true) = iteration_times
+                .front()
+                .map(|t| t.elapsed() > Duration::from_secs(1))
+            {
+                iteration_times.pop_front();
+            }
+            tps_sender.send(iteration_times.len() as f32);
+
+            next_iteration += Duration::from_secs(1).div_f32(ui_config.ticks_per_second());
+
+            state.tick(event_receiver.try_iter());
+            state_sender.send(state.clone());
+
+            if next_iteration > Instant::now() {
+                std::thread::sleep(next_iteration - Instant::now());
+            } else {
+                next_iteration = Instant::now();
+            }
+        }
+    });
+
+    for _ in 0..1000 {
+        event_sender
+            .send(Event::AddParticle(Particle::new(
+                Location::new(32., 32.),
+                Vector::new(0., 1.),
+            )))
+            .unwrap();
+    }
+
+    let mut state = state_receiver
+        .get_if_new()
+        .expect("State should not have been seen yet.");
+
+    let mut tps = 0.;
 
     loop {
-        events.extend(ui_state.handle_input());
-
-        tick.elapse_time(
-            time::get_frame_time() * ui_config.ticks_per_second(),
-            || state.tick(events.drain(..)),
-        );
+        for event in ui_state.handle_input() {
+            event_sender.send(event).unwrap();
+        }
 
         ui_state.update_window_info();
 
-        draw::draw(&state, &ui_state);
+        state = state_receiver.get_if_new().unwrap_or(state);
+        tps = tps_receiver.get() * 0.2 + tps * 0.8;
+
+        draw::draw(&state, &ui_state, tps as u32);
 
         window::next_frame().await
     }
